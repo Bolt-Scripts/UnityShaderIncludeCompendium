2f4df7970c6f6dc20f9cb1d5fae5a49c3275ad04
BoltManGuy <boltsoft@gmail.com>
2019-09-03 11:46:46 -0400

    Fixed dataset and many style improvements

--- js/Index.js
+++ js/Index.js
@@ -6,9 +6,9 @@ function Index() {
 
     searchPhrase = "";
 
-    GetClass = function(d){
+    GetClass = function (d) {
 
-        if(d == "comment"){
+        if (d == "comment") {
             return "code-comment";
         }
 
@@ -17,8 +17,8 @@ function Index() {
 
     let lastFilter = "";
     let filterTimeout;
-    CheckFilter = function(d, e){        
-        if(lastFilter == searchPhrase) return;
+    CheckFilter = function (d, e) {
+        if (lastFilter == searchPhrase) return;
         lastFilter = searchPhrase;
 
         clearTimeout(filterTimeout);
@@ -26,19 +26,19 @@ function Index() {
         setTimeout(() => UpdateItems(filter), 500);
     }
 
-    UpdateItems = function(filter){
-        if(filter == ""){
+    UpdateItems = function (filter) {
+        if (filter == "") {
             searchResults([]);
             return;
         }
         let lowerFilter = filter.toLowerCase();
         let filteredItems = allItems.filter(x => x.searchBy.toLowerCase().includes(lowerFilter));
 
-        if(filteredItems.length > 50){
+        if (filteredItems.length > 50) {
             filteredItems = filteredItems.slice(0, 50);
         }
 
-        filteredItems.sort((a,b)=> a.searchBy.length - b.searchBy.length);
+        filteredItems.sort((a, b) => a.searchBy.length - b.searchBy.length);
         // filteredItems = filteredItems.sort((a,b)=> a.searchBy > b.searchBy);
 
         searchResults(filteredItems);
@@ -46,47 +46,49 @@ function Index() {
     }
 
     allItems = [];
-    FixItems = function(){
+    FixItems = function () {
         for (var file of cgincData.files) {
             // console.log(file);
-            for(var prop in file){
+            for (var prop in file) {
                 if (Object.prototype.hasOwnProperty.call(file, prop)) {
-                    if(typeof(file[prop]) != "object"){
+                    if (typeof (file[prop]) != "object") {
                         continue;
                     }
 
                     // do stuff
-                    for(var item of file[prop]){
+                    for (var item of file[prop]) {
                         let newItem = {};
                         newItem.properties = {};
                         newItem.propNames = [];
 
-                        for(var itemProp in item){
+                        for (var itemProp in item) {
                             if (itemProp != "code" && Object.prototype.hasOwnProperty.call(item, itemProp)) {
-                                if(itemProp == "comment" || itemProp == "modifiers"){                                    
-                                    if(item[itemProp].trim() == "") continue;
+                                if (itemProp == "comment" || itemProp == "modifiers") {
+                                    if (item[itemProp].trim() == "") continue;
                                 }
 
                                 newItem.properties[itemProp] = item[itemProp];
 
-                                if(itemProp == "parameters"){
+                                if (itemProp == "parameters") {
                                     newItem.properties[itemProp] = "(" + newItem.properties[itemProp] + ")";
                                 }
 
-
-                                newItem.propNames.push(itemProp);
+                                if (itemProp != "lineNum")
+                                    newItem.propNames.push(itemProp);
                             }
                         }
 
                         newItem.properties["file"] = file.file;
                         newItem.propNames.push("file");
+                        newItem.propNames.push("lineNum");
+
                         newItem.code = item.code;
                         newItem.searchBy = item.name ? item.name : item.code;
                         newItem.codeShown = ko.observable(false);
                         allItems.push(newItem);
                     }
                 }
-                
+
             }
         }
     }
